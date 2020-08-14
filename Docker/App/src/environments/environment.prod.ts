d9e935641ac99fd8f8f852db43ba39256b77734e
Paul Stempel <stempel.paul@googlemail.com>
2020-08-14 22:36:29 +0200

    docker

---
+++ Docker/App/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  application:
+  {
+    name: 'angular-starter',
+    angular: 'Angular 10.0.8',
+    bootstrap: 'Bootstrap 4.5.1',
+    fontawesome: 'Font Awesome 5.14.0',
+  }
+};
---
+++ Docker/App/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  application:
+  {
+    name: 'angular-starter',
+    angular: 'Angular 10.0.8',
+    bootstrap: 'Bootstrap 4.5.1',
+    fontawesome: 'Font Awesome 5.14.0',
+  }
+};
